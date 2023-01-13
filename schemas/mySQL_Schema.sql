89c8b9808ef181f27c7d6a5dd0e3f208b9b5f76a
Josh Krebs <joddykrebs4@gmail.com>
2023-01-13 12:22:48 -0800

    first schema mySQL

---
+++ schemas/mySQL_Schema.sql
@@ -0,0 +1,127 @@
+-- CREATE DATABASE chat;
+
+-- USE chat;
+
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+--   id int NOT NULL AUTO_INCREMENT,
+--   userid int NOT NULL,
+--   text varchar(200) NOT NULL,
+--   roomname varchar(20),
+--   PRIMARY KEY (ID)
+-- );
+
+-- /* Create other tables and define schemas for them here! */
+
+-- CREATE TABLE users (
+--   id int NOT NULL AUTO_INCREMENT,
+--   username varchar(40) NOT NULL,
+--   PRIMARY KEY (ID)
+-- )
+
+--  data get /reviews:
+
+-- {
+--   "product": "2",
+--   "page": 0,
+--   "count": 5,
+--   "results": [
+--     {
+--       "review_id": 5,
+--       "rating": 3,
+--       "summary": "I'm enjoying wearing these shades",
+--       "recommend": false,
+--       "response": null,
+--       "body": "Comfortable and practical.",
+--       "date": "2019-04-14T00:00:00.000Z",
+--       "reviewer_name": "shortandsweeet",
+--       "helpfulness": 5,
+--       "photos": [{
+--           "id": 1,
+--           "url": "urlplaceholder/review_5_photo_number_1.jpg"
+--         },
+--         {
+--           "id": 2,
+--           "url": "urlplaceholder/review_5_photo_number_2.jpg"
+--         },
+--         // ...
+--       ]
+--     },
+--     {
+--       "review_id": 3,
+--       "rating": 4,
+--       "summary": "I am liking these glasses",
+--       "recommend": false,
+--       "response": "Glad you're enjoying the product!",
+--       "body": "They are very dark. But that's good because I'm in very sunny spots",
+--       "date": "2019-06-23T00:00:00.000Z",
+--       "reviewer_name": "bigbrotherbenjamin",
+--       "helpfulness": 5,
+--       "photos": [],
+--     },
+--     // ...
+--   ]
+-- }
+
+--  metaData:
+
+-- {
+--   "product_id": "2",
+--   "ratings": {
+--     2: 1,
+--     3: 1,
+--     4: 2,
+--     // ...
+--   },
+--   "recommended": {
+--     0: 5
+--     // ...
+--   },
+--   "characteristics": {
+--     "Size": {
+--       "id": 14,
+--       "value": "4.0000"
+--     },
+--     "Width": {
+--       "id": 15,
+--       "value": "3.5000"
+--     },
+--     "Comfort": {
+--       "id": 16,
+--       "value": "4.0000"
+--     },
+--     // ...
+-- }
+
+
+CREATE DATABASE reviews;
+
+USE reviews;
+
+CREATE TABLE reviewData {
+  id int NOT NULL AUTO_INCREMENT,
+  product int NOT NULL,
+  page int NOT NULL,
+  count int NOT NULL,
+  PRIMARY KEY (product)
+};
+
+CREATE TABLE results {
+  id int NOT NULL AUTO_INCREMENT,
+  review_id int NOT NULL,
+  rating int NOT NULL,
+  summary VARCHAR(200),
+  recommended VARCHAR(10),
+  response VARCHAR(200),
+  body VARCHAR(200),
+  date DATE(50),
+  reviewer_name VARCHAR(50) NOT NULL,
+  helpfulness VARCHAR(10)
+  FOREIGN KEY (id) REFERENCES reviewData(product)
+};
+
+CREATE TABLE photos {
+  id int NOT NULL AUTO_INCREMENT,
+  url VARCHAR(250),
+  FOREIGN KEY (id) REFERENCES results(review_id)
+};
