b866f224ccb267f2b485f4c41a145b81cba27239
Josh Krebs <joddykrebs4@gmail.com>
2023-01-25 11:21:12 -0800

    creating results objects for metaData

--- model/getMetaData.js
+++ model/getMetaData.js
@@ -15,6 +15,11 @@ module.exports = {
     // - ratings: from results table
     // - recommended: from results table
     // - characteristics: from characteristics and characteristic reviews
+    // SELECT *
+    // FROM characteristics
+    // JOIN characteristics_reviews
+    // ON characteristics.id = characteristics_reviews.characteristic_id
+    // WHERE characteristics.product_id = 71669;
 
     // RATINGS AND REC Q: `SELECT rating, recommend FROM results WHERE product_id = ${product_id}`
     // still need to make data into correct shape
@@ -43,32 +48,102 @@ module.exports = {
     //hyp:
     // characteristics.id = characteristics_reviews.characteristic_id
 
-    select * from characteristics where id = 123; //  123 |         34 | "Comfort"
-    select * from characteristics_reviews where characteristic_id = 123;
 
-    SELECT *
-    FROM characteristics
-    JOIN characteristics_reviews
-    ON characteristics.id = characteristics_reviews.characteristic_id
-    WHERE characteristics.product_id = 71669;
-
-
-
-
-    select * from characteristics where product_id = 71669;
-    select * from characteristics_reviews where id = 1;
-
-    select * from characteristics where id = 239612;
-
-
-
-    let query = `SELECT rating, recommend FROM results WHERE product_id = ${product_id}`;
-    return new Promise((resolve, reject) => {
-      db.connection.query(query, (err, results, fields) => {
+    let ratQuery = `SELECT rating, recommend FROM results WHERE product_id = ${product_id}`;
+    const ratsAndRec = new Promise((resolve , reject) => {
+      db.connection.query(ratQuery, (err, results, fields) => {
         if (err) reject(err);
 
         resolve(results);
       })
     })
+
+    let charQuery =
+    `SELECT *
+    FROM characteristics
+    JOIN characteristics_reviews
+    ON characteristics.id = characteristics_reviews.characteristic_id
+    WHERE characteristics.product_id = 71669`
+
+    const chars = new Promise((resolve , reject) => {
+      db.connection.query(charQuery, (err, results, fields) => {
+        if (err) reject(err);
+
+        resolve(results);
+      })
+    })
+
+
+
+    Promise.all([ratsAndRec, chars])
+      .then((data) => {
+        // console.log('data from promise.all, ', data);
+
+        // consider adding different middleware files
+        // transform data:
+
+
+
+        let ratingObj = {};
+        let recObj = {};
+        let charsObj = {};
+
+        let ratsAndRecs = data[0];
+        let charsRes = data[1];
+
+        ratsAndRecs.forEach((obj) => {
+          let curRating = obj.rating;
+          let curRec = obj.recommend;
+
+          if (ratingObj[curRating] === undefined) {
+            ratingObj[curRating]= 1;
+          } else {
+            ratingObj[curRating]++;
+          }
+
+          if (recObj[curRec] === undefined) {
+            recObj[curRec] = 1;
+          } else {
+            recObj[curRec]++;
+          }
+
+        })
+
+        // the value of each one is an average:
+
+        // need to accumulate the average of the ratings on each object
+
+        charsRes.forEach((obj) => {
+          console.log('chars objs', obj);
+          let curChar = obj.name;
+
+          delete obj.name;
+          charsObj[curChar] = obj;
+          // charsObj.curChar = obj;
+        })
+
+        console.log('charsObj', charsObj)
+
+        // console.log('rating obj', ratingObj)
+        // console.log('rec obj', recObj)
+
+
+      })
+      .catch((err) => {
+        console.log('metadata prmise/all  error ', err);
+      })
+
+
+    // let query = `SELECT rating, recommend FROM results WHERE product_id = ${product_id}`;
+    // return new Promise((resolve, reject) => {
+    //   db.connection.query(query, (err, results, fields) => {
+    //     if (err) reject(err);
+
+    //     resolve(results);
+    //   })
+    // })
+
+
+
   }
 }
